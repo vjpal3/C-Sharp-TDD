ddd5152bee8ec81624401f24520f27b545bb487a
vjpal3 <vrishalipal@yahoo.com>
2019-09-20 17:25:42 -0400

    Initial commit

---
+++ AdvancedTDD/TestDoubles/WorkingStatistics.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedTDD.TestDoubles
+{
+    public class WorkingStatistics
+    {
+        public decimal HourSalary { get; set; }
+        public int WorkingHours { get; set; }
+        public decimal MonthSalary { get; set; }
+        public bool PayHourly { get; set; }
+    }
+}
---
+++ AdvancedTdd.Tests/TestDoubles/CustomerTests.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace AdvancedTdd.Tests.TestDoubles
+{
+    [TestFixture]
+    class CustomerTests
+    {
+
+    }
+}
