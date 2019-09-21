dd414bd2ac956659618f48024467ccd55dfc5673
vjpal3 <vrishalipal@yahoo.com>
2019-09-20 20:25:01 -0400

    Use NSubstitute Mocking Framework to write tests.

--- AdvancedTdd.Tests/TestDoubles/CustomerTests.cs
+++ AdvancedTdd.Tests/TestDoubles/CustomerTests.cs
@@ -1,10 +1,48 @@
 ﻿using System;
 using NUnit.Framework;
 using AdvancedTDD.TestDoubles;
+using NSubstitute;
 
 namespace AdvancedTdd.Tests.TestDoubles
 {
     [TestFixture]
+    class CustomerTestsWithMockingFramework
+    {
+        [Test]
+        public void CalculateWage_HourlyPaid_ReturnsCorrectValue()
+        {
+            var gateway = Substitute.For<IDbGateway>();
+            var workingStatistics = new WorkingStatistics()
+            {
+                PayHourly = true,
+                HourSalary = 100,
+                WorkingHours = 10
+            };
+
+            const int anyId = 1;
+            gateway.GetWorkingStatistics(anyId).ReturnsForAnyArgs(workingStatistics);
+            
+            var sut = new Customer(Substitute.For<ILogger>(), gateway);
+
+            decimal actual = sut.CalculateWage(anyId);
+            const decimal expectedWage = 100 * 10;
+
+            Assert.That(actual, Is.EqualTo(expectedWage).Within(0.1));
+        }
+
+        [Test]
+        public void CalculateWage_PassesCorrectId()
+        {
+            const int id = 1;
+            var gateway = Substitute.For<IDbGateway>();
+
+            var sut = new Customer(Substitute.For<ILogger>(), gateway);
+            gateway.GetWorkingStatistics(id).ReturnsForAnyArgs(new WorkingStatistics());
+            sut.CalculateWage(id);
+            gateway.Received().GetWorkingStatistics(id);
+        }
+    }
+
     class CustomerTests
     {
         [Test]
